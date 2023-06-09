e5b1eeee83830d25a50c106891f0e0d9ed66e004
LuxuryScripts <110750740+LuxuryScripts@users.noreply.github.com>
2023-06-09 20:01:37 +0200

    Add files via upload

---
+++ OX_Inventory/Items/items-de.lua
@@ -0,0 +1,7 @@
+["umbrella"] = {
+		label = "Regenschirm",
+		weight = 500,
+		stack = false,
+		close = true,
+		allowArmed = false,
+	},
---
+++ OX_Inventory/Items/items-en.lua
@@ -0,0 +1,7 @@
+["umbrella"] = {
+		label = "Umbrella",
+		weight = 500,
+		stack = false,
+		close = true,
+		allowArmed = false,
+	},
